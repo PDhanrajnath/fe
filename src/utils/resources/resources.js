428dcc0b927035a0c82f408a6a090c1448dad749
PDhanrajnath <porikadhanrajnath@gmail.com>
2021-10-05 20:42:11 +0530

    localhost

--- src/utils/resources/resources.js
+++ src/utils/resources/resources.js
@@ -1,4 +1,4 @@
-const { BASE_URL } = window.env || {BASE_URL : 'http://localhost:8080'};
+const { BASE_URL } = window.env || {BASE_URL : 'http://bc15.dev-apps.io:32002'};
 
 export const apis = {
   GET_SKILLS: `${BASE_URL}/api/skills`,
